580748b0f0c39665f78ef2c228804b9eb2e565e6
idOgItt <sergeylkd@bk.ru>
2025-07-20 20:37:46 +0300

    TODO

---
+++ src/main/java/ru/mai/lessons/rpks/impl/ServiceEnrichment.java
@@ -0,0 +1,11 @@
+package ru.mai.lessons.rpks.impl;
+
+import com.typesafe.config.Config;
+import ru.mai.lessons.rpks.Service;
+
+public class ServiceEnrichment implements Service {
+    @Override
+    public void start(Config config) {
+        // написать код реализации сервиса обогащения
+    }
+}
